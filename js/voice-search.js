04ec12b13c2f4b202817a0bfa8a8b73de8f097e7
raul torres <rtorres.dev2@gmail.com>
2024-04-29 15:51:57 +0200

    optimizando el codigo js

--- js/voice-search.js
+++ js/voice-search.js
@@ -20,7 +20,7 @@ const voiceRecognition = () =>{
 	if (microAceptado == false) {
 	window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
 	if (!'SpeechRecognition' in window) {
-		alert("que pena, no podes usar la API")
+		alert("que pena, no podes usar la API, cabio.")
 }  
 	}
 	document.querySelector(".voice-search__result-text").innerHTML = "Habla ahora";
